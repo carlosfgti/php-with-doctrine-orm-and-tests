158fbf7cd0c02c7c0650c138007f8174a4582237
Carlos Ferreira <carlos@especializati.com.br>
2022-08-17 10:47:48 -0300

    feat: define bootstrap application

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 use Src\Example;
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap/bootstrap.php';
 
 $example = new Example();
 var_dump($example->test());
