c50f0593f1606a1f6c156f724e39a3f857824bcf
Carlos Ferreira <carlos@especializati.com.br>
2022-08-18 11:48:48 -0300

    Install and config Dotenv

--- public/index.php
+++ public/index.php
@@ -4,9 +4,13 @@ declare(strict_types = 1);
 
 require_once __DIR__ . '/../bootstrap/bootstrap.php';
 
+use Dotenv\Dotenv;
 use Doctrine\ORM\ORMSetup;
 use Doctrine\ORM\EntityManager;
 use Src\Entities\Course;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
 
 $paths = [__DIR__."/../Entities"];
 $isDevMode = true;
@@ -16,11 +20,11 @@ $useSimpleAnnotationReader = false;
 $config = ORMSetup::createAnnotationMetadataConfiguration($paths, $isDevMode, $proxyDir, $cache);
 
 $connection = [
-    'dbname' => 'academy',
-    'user' => 'root',
-    'password' => 'root',
-    'host' => 'db',
-    'driver' => 'pdo_mysql'
+    'dbname' => $_ENV['DB_NAME'],
+    'user' => $_ENV['DB_USER'],
+    'password' => $_ENV['DB_PASSWORD'],
+    'host' => $_ENV['DB_HOST'],
+    'driver' => $_ENV['DB_DRIVE'] ?? 'pdo_mysql'
 ];
 // database configuration parameters
 // $connection = [
