1f4c241639a28346f6895ead6643181652f188c1
Carlos Ferreira <carlos@especializati.com.br>
2022-10-10 12:40:26 -0300

    refactor: clean file

--- public/index.php
+++ public/index.php
@@ -5,38 +5,6 @@ declare(strict_types = 1);
 require_once __DIR__ . '/../bootstrap/bootstrap.php';
 
 use Dotenv\Dotenv;
-use Doctrine\ORM\ORMSetup;
-use Doctrine\ORM\EntityManager;
-use Src\Entities\Course;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
-
-$paths = [__DIR__."/../src/Entities"];
-$isDevMode = (bool) $_ENV['IS_DEV'];
-$config = ORMSetup::createAnnotationMetadataConfiguration($paths, $isDevMode);
-
-$connection = [
-    'dbname' => $_ENV['DB_NAME'],
-    'user' => $_ENV['DB_USER'],
-    'password' => $_ENV['DB_PASSWORD'],
-    'host' => $_ENV['DB_HOST'],
-    'driver' => $_ENV['DB_DRIVE'] ?? 'pdo_mysql'
-];
-// database configuration parameters
-// $connection = [
-//     'driver' => 'pdo_sqlite',
-//     'path' => __DIR__ . '/db.sqlite',
-// ];
-
-$entityManager = EntityManager::create($connection, $config);
-$conn = $entityManager->getConnection();
-$conn->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
-
-// $course = new Course;
-// $course->setName('test');
-// $course->setImage('image.png');
-// $course->setDescription('desc');
-// $entityManager->persist($course);
-// $entityManager->flush();
-
