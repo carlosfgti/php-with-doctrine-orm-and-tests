a39248778f37c932c572877f39d5d4b840762b39
Carlos Ferreira <carlos@especializati.com.br>
2022-10-11 10:42:51 -0300

    fix: create variable entityManager

--- cli-config.php
+++ cli-config.php
@@ -2,8 +2,11 @@
 
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+use Src\Helper\EntityManagerCreator;
 
 require __DIR__ . '/public/index.php';
+
+$entityManager = EntityManagerCreator::createEntityManager();
 
 ConsoleRunner::run(
     new SingleManagerProvider($entityManager)
