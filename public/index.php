d67390e30897be7a5a88d46d131b4bdec1b07de7
Carlos Ferreira <carlos@especializati.com.br>
2022-10-13 10:31:29 -0300

    feat: config routes

--- public/index.php
+++ public/index.php
@@ -5,6 +5,21 @@ declare(strict_types = 1);
 require_once __DIR__ . '/../bootstrap/bootstrap.php';
 
 use Dotenv\Dotenv;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
+$app = AppFactory::create();
+
+/**
+ * Routes
+ */
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('<a href="/hello/world">Try /hello/world</a>');
+    return $response;
+});
+
+$app->run();
