e804d67a3461058f37b1adc4bec57427f0d4ad47
Carlos Ferreira <carlos@especializati.com.br>
2022-10-13 10:48:45 -0300

    feat: response specializations with courses format json

--- public/index.php
+++ public/index.php
@@ -8,9 +8,14 @@ use Dotenv\Dotenv;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Src\Entities\Course;
+use Src\Entities\Specialization;
+use Src\Helper\EntityManagerCreator;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
+
+$entityManager = EntityManagerCreator::createEntityManager();
 
 $app = AppFactory::create();
 
@@ -19,6 +24,33 @@ $app = AppFactory::create();
  */
 $app->get('/', function (Request $request, Response $response) {
     $data = ['success' => true];
+    $payload = json_encode($data);
+
+    $response->getBody()->write($payload);
+    return $response
+            ->withHeader('Content-Type', 'application/json')
+            ->withStatus(200);
+});
+
+$app->get('/specializations', function (Request $request, Response $response) use ($entityManager) {
+    $repository = $entityManager->getRepository(Specialization::class);
+    $specializations = $repository->getSpecializationsWithCourses();
+
+    /** @var Specialization[] $specializations */
+    $data = [];
+    foreach ($specializations as $key => $specialization) {
+        $courses = [];
+        /** @var Course[] $courses */
+        foreach ($specialization->courses() as $course) {
+            $courses[] = [
+                'name' => $course->getName(),
+            ];
+        }
+        $data[$key] = [
+            'name' => $specialization->getName(),
+            'courses' => $courses,
+        ];
+    }
     $payload = json_encode($data);
 
     $response->getBody()->write($payload);
